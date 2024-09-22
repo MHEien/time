dc9a0058c1b5203a247580bdee39bf57bdb55d33
MHEien <markushei@hotmail.no>
2024-09-22 23:16:08 +0200

    vercel postgres

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,6 @@
-
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import { env } from "@/env";
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from '@vercel/postgres';
 import * as schema from "./schema";
 
-export const connection = postgres(env.DATABASE_URL, {
-  max_lifetime: 10, // Remove this line if you're deploying to Docker / VPS
-  // idle_timeout: 20, // Uncomment this line if you're deploying to Docker / VPS
-});
 
-export const db = drizzle(connection, { schema });
+export const db = drizzle(sql, { schema });
