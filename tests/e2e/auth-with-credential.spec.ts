aab01e01989e6f577f42fd7ffc627e8bb386e66e
MHEien <markushei@hotmail.no>
2024-09-22 20:50:59 +0200

    Added login to header

--- tests/e2e/auth-with-credential.spec.ts
+++ tests/e2e/auth-with-credential.spec.ts
@@ -16,30 +16,31 @@ test.beforeAll(() => {
 test.describe("signup and login", () => {
   test("signup", async ({ page }) => {
     await page.goto("/");
-    await page.getByText("login").click();
-    await page.getByText(/sign up/i).click();
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('link', { name: /sign up/i }).click();
     await page.waitForURL("/signup");
     await page.getByLabel("Email").fill(testUser.email);
     await page.getByLabel("Password").fill(testUser.password);
-    await page.getByLabel("submit-btn").click();
+    await page.getByRole('button', { name: 'Submit' }).click();
     await page.waitForURL("/verify-email");
     const data = readFileSync("application.log", { encoding: "utf-8" });
     const code = extractLastCode(data);
     expect(code).not.toBeNull();
     await page.getByLabel("Verification Code").fill(code!);
-    await page.getByLabel("submit-btn").click();
+    await page.getByRole('button', { name: 'Submit' }).click();
     await page.waitForURL("/dashboard");
   });
+
   test("login and logout", async ({ page }) => {
     await page.goto("/");
-    await page.getByText("login").click();
+    await page.getByRole('link', { name: 'Login' }).click();
     await page.getByLabel("Email").fill(testUser.email);
     await page.getByLabel("Password").fill(testUser.password);
-    await page.getByLabel("submit-btn").click();
+    await page.getByRole('button', { name: 'Submit' }).click();
     await page.waitForURL("/dashboard");
     await page.getByAltText("Avatar").click();
-    await page.getByText("Sign out").click();
-    await page.getByText("Continue").click();
+    await page.getByRole('button', { name: 'Sign out' }).click();
+    await page.getByRole('button', { name: 'Continue' }).click();
     await page.waitForURL("/");
   });
 });
