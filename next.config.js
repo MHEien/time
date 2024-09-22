191cc772d7a3d55f6fcdb154d3dfad83f7ee401e
MHEien <markushei@hotmail.no>
2024-09-22 21:45:37 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 await import("./src/env.js");
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import("next").NextConfig} */
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
 const config = {
     images: {
         remotePatterns: [
@@ -14,8 +19,6 @@ const config = {
       },
   }
 
-  if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-  }
+
     
 export default config;
